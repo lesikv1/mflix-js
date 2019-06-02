c0af3cf309180e3d4a7718ee84b719aa70326211
Alexey Onufriev <aleksonufriev@gmail.com>
2019-06-02 19:16:24 +0300

    task about Create/Update Comments

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -43,9 +43,14 @@ export default class CommentsDAO {
    */
   static async addComment(movieId, user, comment, date) {
     try {
-      // TODO Ticket: Create/Update Comments
-      // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      // here's how the commentDoc is constructed
+      const commentDoc = {
+        name: user.name,
+        email: user.email,
+        movie_id: ObjectId(movieId),
+        text: comment,
+        date: date,
+      }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -66,12 +71,10 @@ export default class CommentsDAO {
    */
   static async updateComment(commentId, userEmail, text, date) {
     try {
-      // TODO Ticket: Create/Update Comments
-      // Use the commentId and userEmail to select the proper comment, then
-      // update the "text" and "date" fields of the selected comment.
+      // here's how the update is performed
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { _id: ObjectId(commentId), email: userEmail },
+        { $set: { text, date } },
       )
 
       return updateResponse
